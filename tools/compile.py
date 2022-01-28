c23acdae565c3027217771b0a9b7d09fdae6acd2
Gabe Fierro <gtfierro@mines.edu>
2022-01-28 14:47:53 -0700

    fix compile path

--- tools/compile.py
+++ tools/compile.py
@@ -2,6 +2,6 @@ import ontoenv
 import rdflib
 env = ontoenv.OntoEnv()
 graph = rdflib.Graph()
-graph.parse("all.ttl", format="ttl")
+graph.parse("model/all.ttl", format="ttl")
 env.import_dependencies(graph)
 graph.serialize("build/ref-schema.ttl", format="ttl")
