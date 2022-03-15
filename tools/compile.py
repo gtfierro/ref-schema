0f44f53f62d40ce9474dc46a620e6705caebaf86
Gabe Fierro <gtfierro@mines.edu>
2022-03-15 00:20:27 -0600

    remove duplicate owl:Ontology definitions

--- tools/compile.py
+++ tools/compile.py
@@ -1,7 +1,10 @@
 import ontoenv
 import rdflib
+REF = rdflib.URIRef("https://brickschema.org/schema/Brick/ref")
 env = ontoenv.OntoEnv()
 graph = rdflib.Graph()
 graph.parse("model/all.ttl", format="ttl")
 env.import_dependencies(graph)
+graph.remove((None, rdflib.RDF.type, rdflib.OWL.Ontology))
+graph.add((REF, rdflib.RDF.type, rdflib.OWL.Ontology))
 graph.serialize("build/ref-schema.ttl", format="ttl")
