df385eb41d21ce655d6f97dda4737c02c359ccda
Gabe Fierro <gtfierro@mines.edu>
2022-03-15 00:22:58 -0600

    more cleaning up of imports on compilation

--- tools/compile.py
+++ tools/compile.py
@@ -5,6 +5,11 @@ env = ontoenv.OntoEnv()
 graph = rdflib.Graph()
 graph.parse("model/all.ttl", format="ttl")
 env.import_dependencies(graph)
+# clean up ontology defs and their imports that aren't the core 'ref' schema
 graph.remove((None, rdflib.RDF.type, rdflib.OWL.Ontology))
 graph.add((REF, rdflib.RDF.type, rdflib.OWL.Ontology))
+for ont, imp in graph.subject_objects(rdflib.OWL.imports):
+    if ont != REF:
+        graph.remove((ont, rdflib.OWL.imports, imp))
+
 graph.serialize("build/ref-schema.ttl", format="ttl")
